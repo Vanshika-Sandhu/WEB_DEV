851d7ef0e15a0e80b7953e655de58a4668406342
= <=>
2021-02-24 00:38:33 +0530

    user created!

--- lec11_InstaClone/backend/controller/userController.js
+++ lec11_InstaClone/backend/controller/userController.js
@@ -1,9 +1,19 @@
-const { Mongoose, mongo } = require("mongoose");
-
-const {mongoose} = require("../model/db");
+const userModel = require("../model/userModel");
 
 function createUser(req , res){
-    console.log("Inside createUser");
+  //console.log("Inside createUser function!!!");
+  userModel.create({
+    name: "Michael Scott",
+    username:"call_me_mike",
+    bio:"THAT'S WHAT SHE SAID!",
+    email:"littlekidlover@gmail.com",
+    password:"feelingHOTHOTHOT"
+  })
+  .then(function(createdUser){
+    res.json({
+        createdUser
+    });
+  });
 }
 
 module.exports.createUser = createUser;
