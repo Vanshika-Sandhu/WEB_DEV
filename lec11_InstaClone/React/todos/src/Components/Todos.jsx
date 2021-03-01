d73137cfed50b4ef71cad55696fa486bede29cb8
= <=>
2021-03-02 00:32:26 +0530

    todo intro

---
+++ lec11_InstaClone/React/todos/src/Components/Todos.jsx
@@ -0,0 +1,18 @@
+// imrc => import React, { Component } from 'react';
+import React, { Component } from 'react';
+
+// cc => class based component
+class Todos extends Component {
+    
+    //constructor missing 
+    //super missing 
+    // this can be omitted as the code understands it has to call constructor and super
+    // this is possible only because of JSX
+
+    state = {  }
+    render() { 
+        return ( <h1>Hello from Todos component!</h1> );
+    }
+}
+ 
+export default Todos;
