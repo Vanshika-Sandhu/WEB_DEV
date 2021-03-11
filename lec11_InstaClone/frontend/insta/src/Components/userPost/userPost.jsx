d400f2eeb0acc913643d2f7ec40010cf3e113a52
= <=>
2021-03-11 13:26:48 +0530

    liked post complete

--- lec11_InstaClone/frontend/insta/src/Components/userPost/userPost.jsx
+++ lec11_InstaClone/frontend/insta/src/Components/userPost/userPost.jsx
@@ -4,29 +4,55 @@ import './userPost.css';
 class UserPost extends Component {
     state = { 
         caption:"",
-        postImage:""
+        postImage:"",
+        postId:"",
+        likes:[],
+        comments:[]
      }
 
      componentDidMount(){
         let caption = this.props.post.caption;
         let postImage = this.props.post.postImage;
         let postId = this.props.post["_id"];
+        let likes = this.props.post.likes;
+        let comments = this.props.post.comments;
         this.setState({
             postImage,
             caption,
-            postId
+            postId,
+            likes,
+            comments
         })
      }
 
 
     render() { 
-        let {caption, postImage} = this.state;
+        let {caption, postImage , likes, comments} = this.state;
         return (  
             <div className="profile-user-post">
                 <img src={postImage} alt=""/>
                 <div className="profile-caption-footer">
-                    <div className="profile-caption-username">Caption</div>
+                    <div className="profile-post-reaction-tab">
+                        <div className="post-likes">{likes.length} likes</div>
+                        <div className="post-comments">{comments.length} comments</div>
+                    </div>
                     <div className="profile-post-image-caption">{caption}</div>
+                    <div className="profile-comments">
+                    {
+                         comments.length ?(
+                            comments.map(commentInfo=>{
+                            return <div className="profile-comment-info" key={commentInfo["id"]}>
+                                        <div className="profile-user-commented username">{commentInfo.user}:</div>
+                                        <div className="profile-comment">{commentInfo.comment}</div>
+                                    </div>
+                                })
+                            )
+                            :
+                            <div className="profile-comment-info">
+
+                            </div>
+                    }
+                    </div>
                 </div>
             </div>
         );
