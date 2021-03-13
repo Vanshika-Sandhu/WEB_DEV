2266ec1fb0c6c7e044f4bdbcafb40497171d0c3d
= <=>
2021-03-13 19:43:51 +0530

    request send implemented

--- lec11_InstaClone/frontend/insta/src/Components/HomeProfile/HomeProfile.jsx
+++ lec11_InstaClone/frontend/insta/src/Components/HomeProfile/HomeProfile.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import "./HomeProfile.css";
 import {Link} from "react-router-dom";
+import Suggestion from '../Suggestion/Suggestion';
 
 class HomeProfile extends Component {
     state = { 
@@ -31,35 +32,6 @@ class HomeProfile extends Component {
         })
      }
 
-     onCancelRequestHandler = (suggestion)=>{
-        console.log("inside cancel request handler");
-        let uid = this.props.user["_id"];
-        let followId = suggestion["_id"];
-        axios.delete(`/api/request/cancel/${followId}/${uid}`).then(obj=>{
-            if(obj.data.message === "Request cancelled successfully" ){
-                console.log("Request cancelled successfully");
-                this.setState({
-                    isRequestSent:false
-                });
-            }
-        })
-    }
-
-    sendRequestHandler=(suggestion)=>{
-        let uid = this.props.user["_id"];
-        console.log("Inside send request handler");
-        let followId = suggestion["_id"];
-        // clickedUser = suggestion;
-        // console.log(clickedUser);
-        axios.post(`/api/request`, {uid, followId}).then( obj =>{
-            console.log(obj);
-            this.componentDidMount();
-            console.log("request sent");
-            this.setState({
-                isRequestSent:true,
-            })
-        });
-    }
 
     render() { 
         let { name , username, profilePic} = this.props.user;
@@ -88,22 +60,7 @@ class HomeProfile extends Component {
                         <div className="suggestion-body">
                             {
                             suggestions.map( suggestionList => {
-                                return <div key={suggestionList["_id"]} className="suggested-user user-info">
-                                    <Link to="/userProfile" style={{ textDecoration: 'none' }} >
-                                    <div className="sugg-img user-image" onClick = {()=>this.onclickHandler(suggestionList)}>
-                                        <img src={suggestionList.profilePic} alt=""/>
-                                    </div>
-                                    </Link>
-                                    <Link to="/userProfile" style={{ textDecoration: 'none' }} >
-                                    <div className="name-info" onClick = {()=>this.onclickHandler(suggestionList)}>
-                                        <div className="username"><strong>{suggestionList.username}</strong></div>
-                                        <div className="name">{suggestionList.name}</div>
-                                    </div>
-                                    </Link>
-                                    <div className="follow" onClick={()=>this.sendRequestHandler(suggestionList)}>
-                                        <i className="fas fa-user-plus" ></i>
-                                    </div>                           
-                                </div>
+                                return <Suggestion suggestion={suggestionList}  user={this.props.user}   UserClickedHandler={this.props.UserClickedHandler}></Suggestion>
                             })
                             }
                         </div> 
